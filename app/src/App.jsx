79d9e76b0d1b22ec7624134db6aac62641705d4a
AdilAhmedShekhani <126003246+AdilAhmedShekhani@users.noreply.github.com>
2024-06-07 10:26:49 +0500

    Register Email Verification Component Created

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -5,6 +5,7 @@ import Support from "./pages/Support/Support";
 import TransactionPage from "./pages/Transaction/Transaction";
 import Signup from "./pages/Auth/Signup/Signup";
 import Signin from "./pages/Auth/Signin/Signin";
+import RegisterEmailVerify from "./pages/Auth/RegisterEmailVerify/RegisterEmailVerify";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -25,6 +26,10 @@ const router = createBrowserRouter([
   {
     path: "/signin",
     element: <Signin />,
+  },
+  {
+    path: "/register-email-verify",
+    element: <RegisterEmailVerify />,
   },
 ]);
 
