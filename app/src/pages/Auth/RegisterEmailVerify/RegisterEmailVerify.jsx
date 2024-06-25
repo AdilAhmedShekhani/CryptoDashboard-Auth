c00675985990e89f0215d6b398e71c026aad1ed9
AdilAhmedShekhani <126003246+AdilAhmedShekhani@users.noreply.github.com>
2024-06-25 15:17:03 +0500

    Email verification card updtaed

--- app/src/pages/Auth/RegisterEmailVerify/RegisterEmailVerify.jsx
+++ app/src/pages/Auth/RegisterEmailVerify/RegisterEmailVerify.jsx
@@ -1,13 +1,14 @@
-import { Box, Button, Card, Icon, Text, VStack } from "@chakra-ui/react";
+import { Box, Button, Card, Center, Icon, Text, VStack } from "@chakra-ui/react";
 import React from "react";
 import { MdEmail } from "react-icons/md";
 
 const RegisterEmailVerify = () => {
   return (
-    <Card>
+    <Center minH="100vh" >
+      <Card>
       <VStack spacing={6}>
         <Icon as={MdEmail} boxSize="48px" color="p.purple" />
-        <Text textStyle="h4" color="p.black">
+        <Text textStyle="h4" fontWeight="medium" color="p.black">
           Email Verification
         </Text>
         <Text textAlign="center" textStyle="p2" color="black.60">
@@ -22,6 +23,7 @@ const RegisterEmailVerify = () => {
         </Button>
       </VStack>
     </Card>
+    </Center>
   );
 };
 
